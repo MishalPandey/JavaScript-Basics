64812031279e1042b23351da2984913f28ad08f0
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2026-01-04 22:57:51 +0530

    Add index.Mod.js for module imports and usage

---
+++ index.Mod.js
@@ -0,0 +1,12 @@
+// Moduleshelp you break down large codebases
+// into smaller, managebles parts
+
+import { add, PI as Pie} from './math.js'
+import sub from './math.js' // yaha {} yeh braket lagana nhi hai kyuki sub functio default me added hai
+
+
+
+console.log(add(5, 3));
+console.log(Pie);
+console.log(sub(5, 3));
+
